dd172036bd03086610af215fbd434cffd139bbd9
div72 <60045611+div72@users.noreply.github.com>
2022-10-11 19:07:49 +0300

    main: create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <SDL.h>
 
-/*
- * Lesson 0: Test to make sure SDL is setup properly
- */
 int main(int argc, char** argv){
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
+
+        SDL_Window* window = SDL_CreateWindow("pegg", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+
+        if (window == nullptr) {
+            std::cerr << "An error occured while creating the window: " << SDL_GetError() << std::endl;
+            return 1;
+        }
 
         while (true) {
             for (SDL_Event event; SDL_PollEvent(&event) != 0;) {
