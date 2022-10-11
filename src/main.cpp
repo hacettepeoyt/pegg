ce7169bc22abd3fa90ed0dc39fa1b093ebf66717
div72 <60045611+div72@users.noreply.github.com>
2022-10-11 19:18:59 +0300

    main: add a FPS limiter

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SDL.h>
 
+static constexpr int64_t fps_limit = 60;
+
 int main(int argc, char** argv){
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
@@ -14,11 +16,19 @@ int main(int argc, char** argv){
             return 1;
         }
 
+        uint64_t last_ticks = SDL_GetTicks64();
         while (true) {
+            int64_t delta = SDL_GetTicks64() - last_ticks;
+            last_ticks = SDL_GetTicks64();
+
             for (SDL_Event event; SDL_PollEvent(&event) != 0;) {
                 if (event.type == SDL_QUIT) {
                     goto end;
                 }
+            }
+
+            if (1000 / fps_limit > delta) {
+                SDL_Delay(1000 / fps_limit - delta);
             }
         }
 end:
