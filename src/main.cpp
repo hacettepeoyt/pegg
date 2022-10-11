2bdaa268443602bfa7b64eedc6d5da6e16e38ca1
div72 <60045611+div72@users.noreply.github.com>
2022-10-11 19:01:51 +0300

    main: implement event loop and handler

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,15 @@ int main(int argc, char** argv){
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
+
+        while (true) {
+            for (SDL_Event event; SDL_PollEvent(&event) != 0;) {
+                if (event.type == SDL_QUIT) {
+                    goto end;
+                }
+            }
+        }
+end:
 	SDL_Quit();
 
 	return 0;
