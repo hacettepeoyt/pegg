3ceb519b3f190b8d0cdc1aaf182cef9f89bb8231
div72 <60045611+div72@users.noreply.github.com>
2022-10-11 20:06:02 +0300

    refactor: consistent indentation

--- src/main.cpp
+++ src/main.cpp
@@ -3,49 +3,49 @@
 
 static constexpr int64_t fps_limit = 60;
 
-int main(int argc, char** argv){
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_EVENTS) != 0){
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
-	}
+int main(int argc, char** argv) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_EVENTS) != 0){
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-        SDL_Window* window = SDL_CreateWindow("pegg", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+    SDL_Window* window = SDL_CreateWindow("pegg", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
 
-        if (window == nullptr) {
-            std::cerr << "An error occured while creating the window: " << SDL_GetError() << std::endl;
-            return 1;
-        }
+    if (window == nullptr) {
+        std::cerr << "An error occured while creating the window: " << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-        SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-        if (renderer == nullptr) {
-            std::cerr << "An error occured while creating the renderer: " << SDL_GetError() << std::endl;
-            return 1;
-        }
+    if (renderer == nullptr) {
+        std::cerr << "An error occured while creating the renderer: " << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
-        uint64_t last_ticks = SDL_GetTicks64();
-        while (true) {
-            int64_t delta = SDL_GetTicks64() - last_ticks;
-            last_ticks = SDL_GetTicks64();
+    uint64_t last_ticks = SDL_GetTicks64();
+    while (true) {
+        int64_t delta = SDL_GetTicks64() - last_ticks;
+        last_ticks = SDL_GetTicks64();
 
-            for (SDL_Event event; SDL_PollEvent(&event) != 0;) {
-                if (event.type == SDL_QUIT) {
-                    goto end;
-                }
-            }
-
-            SDL_RenderClear(renderer);
-
-            SDL_RenderPresent(renderer);
-
-            if (1000 / fps_limit > delta) {
-                SDL_Delay(1000 / fps_limit - delta);
+        for (SDL_Event event; SDL_PollEvent(&event) != 0;) {
+            if (event.type == SDL_QUIT) {
+                goto end;
             }
         }
+
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+
+        if (1000 / fps_limit > delta) {
+            SDL_Delay(1000 / fps_limit - delta);
+        }
+    }
 end:
-	SDL_Quit();
+    SDL_Quit();
 
-	return 0;
+    return 0;
 }
